14aa0dbe4aa49413f654860ea4cd9eae3ffaeca8
서대원 <sdw8001@nate.com>
2018-02-22 17:43:32 +0900

    마이그레이션작업

---
+++ Migrations/201802220807304_surfergraphy.cs
@@ -0,0 +1,194 @@
+namespace SurfergraphyApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class surfergraphy : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AdminUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Password = c.String(),
+                        Name = c.String(),
+                        NickName = c.String(),
+                        Tel = c.String(),
+                        Image = c.String(),
+                        Grade = c.String(),
+                        Deleted = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.PhotoBuyHistories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        PhotoId = c.Int(nullable: false),
+                        Wave = c.Int(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Photos",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        PhotograperId = c.Int(nullable: false),
+                        Name = c.String(),
+                        Url = c.String(),
+                        Place = c.String(),
+                        Wave = c.Int(nullable: false),
+                        MimeType = c.String(),
+                        DimentionWidth = c.Int(nullable: false),
+                        DimentionHeight = c.Int(nullable: false),
+                        Resolution = c.Int(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                        ExpirationDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.PhotoSaveHistories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        PhotoId = c.Int(nullable: false),
+                        PhotoBuyHistoryId = c.Int(nullable: false),
+                        Deleted = c.Boolean(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Purchases",
+                c => new
+                    {
+                        OrderId = c.String(nullable: false, maxLength: 128),
+                        PackageName = c.String(),
+                        ProductId = c.String(),
+                        PurchaseTime = c.Long(nullable: false),
+                        PurchaseState = c.Int(nullable: false),
+                        DeveloperPayload = c.String(),
+                        PurchaseToken = c.String(),
+                    })
+                .PrimaryKey(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.UserPhotoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        PhotoId = c.Int(nullable: false),
+                        PhotoSaveHistoryId = c.Int(nullable: false),
+                        PhotoBuyHistoryId = c.Int(nullable: false),
+                        Deleted = c.Boolean(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        NickName = c.String(),
+                        Wave = c.Int(nullable: false),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.UserPhotoes");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Purchases");
+            DropTable("dbo.PhotoSaveHistories");
+            DropTable("dbo.Photos");
+            DropTable("dbo.PhotoBuyHistories");
+            DropTable("dbo.AdminUsers");
+        }
+    }
+}
